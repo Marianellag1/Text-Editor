490f2fa8251a1efd7333acd901f7d98b01e839eb
Mgamboa <marianellag400@gmail.com>
2022-10-29 19:01:19 -0500

    re-made files src and webpack to see find erro of why install isn't working.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,77 +5,6 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 
-// module.exports = () => {
-//   return {
-//     mode: 'development',
-//     entry: {
-//       main: './src/js/index.js',
-//       install: './src/js/install.js'
-//     },
-//     output: {
-//       filename: '[name].bundle.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-//     // TODO: Add and configure workbox plugins for a service worker and manifest file.-done
-//     plugins: [
-//       // Webpack plugin that generates our html file and injects our bundles. 
-//       new HtmlWebpackPlugin({
-//         template: './index.html',
-//         title: 'Jate Text-Editor'//changed
-//       }),
-     
-//       // Injects our custom service worker
-//       new InjectManifest({
-//         swSrc: './src-sw.js',
-//         swDest: 'src-sw.js',
-//       }),
-
-//       // Creates a manifest.json file.
-//       new WebpackPwaManifest({
-//         fingerprints: false,
-//         inject: true,
-//         name: 'Jate Text-Editor',//changed
-//         short_name: 'Jate',//changed
-//         description: 'PWA text-editor challenge',//changed
-//         background_color: '#225ca3',//change=========
-//         theme_color: '#225ca3',//change=========
-//         start_url: './',
-//         publicPath: './',
-//         icons: [
-//           {
-//             src: path.resolve('src/images/logo.png'),
-//             sizes: [96, 128, 192, 256, 384, 512],
-//             destination: path.join('assets', 'icons'),
-//           },
-//         ],
-//       }),
-//     ],
-    
-//     // TODO: Add CSS loaders and babel to webpack.-done
-//     module: {
-//       // CSS loaders
-//       rules: [
-//         {
-//           test: /\.css$/i,
-//           use: ['style-loader', 'css-loader'],
-//         },
-//         {
-//           test: /\.m?js$/,
-//           exclude: /node_modules/,
-//           // We use babel-loader in order to use ES6.
-//           use: {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env'],
-//               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-//             },
-//           },
-//         },
-//       ],
-//     },
-//   };
-// };
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -87,25 +16,31 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.-done
     plugins: [
+      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'J.A.T.E'
+        title: 'Jate Text-Editor'
       }),
+     
+      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
+
+      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: '',
-        short_name: 'J.A.T.E',
-        description: '',
+        name: 'Jate Text-Editor',
+        short_name: 'Jate',
+        description: 'PWA text-editor',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
@@ -115,7 +50,10 @@ module.exports = () => {
         ],
       }),
     ],
+    
+    // TODO: Add CSS loaders and babel to webpack.-done
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -124,6 +62,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
