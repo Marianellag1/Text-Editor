4de6ba9615701639cfdaa2b6051445c40f354056
Mgamboa <marianellag400@gmail.com>
2022-10-29 19:21:29 -0500

    edited install and databse js to see how they affect install btn that is not working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,97 +1,92 @@
-// import { openDB } from 'idb';
-
-// const initdb = async () =>
-//   openDB('jate', 1, {
-//     // Add our database schema if it has not already been initialized.
-//     upgrade(db) {
-//       if (db.objectStoreNames.contains('jate')) {
-//         console.log('jate database already exists');
-//         return;
-//       }
-//         // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
-//       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-//       console.log('jate database created');
-//     },
-//   });
-
-// // TODO: Add logic to a method that accepts some content and adds it to the database
-// // export const putDb = async (content) => console.error('putDb not implemented');
-
-// export const putDb = async (content) => {
-//   console.log('PUT to the database');
-//   const jateDb = await openDB('jate', 1);
-//   const tx = jateDb.transaction('jate', 'readwrite');
-//   const store = tx.objectStore('jate');
-//   const request = store.put({ id: 1, todo: content });
-//   const result = await request;
-//   console.log('🚀 - data saved to the database', result);
-// };
-
-
-// // TODO: Add logic for a method that gets all the content from the database
-
-// // export const getDb = async () => console.error('getDb not implemented');
-
-// export const getDb = async () => {
-//   console.log('GET from the database');
-
-//   // Create a connection to the database database and version we want to use.
-//   const jateDb = await openDB('jate', 1);
-
-//   // Create a new transaction and specify the database and data privileges.
-//   const tx = jateDb.transaction('jate', 'readonly');
-
-//   // Open up the desired object store.
-//   const store = tx.objectStore('jate');
-
-//   // Use the .getAll() method to get all data in the database.
-//   const request = store.get(1);
-
-//   // Get confirmation of the request.
-//   const result = await request;
-//   console.log('result.value', result);
-//   result 
-//   ? console.log(":rocket: - data retrieved from the database ", result.value)
-//   : console.log(":rocket: - data not found in the database ");
-//   return result?.value
-// };
-
-// initdb();
-
 import { openDB } from 'idb';
+
 const initdb = async () =>
   openDB('jate', 1, {
+    // Add our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+        // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
-// Method that takes some content and adds it to the IndexedDB database using the idb module
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+// export const putDb = async (content) => console.error('putDb not implemented');
+
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const request = store.put({ id: 1, todo: content });
   const result = await request;
-  console.log(':rocket: - data saved to the database', result.value);
+  console.log('🚀 - data saved to the database', result.value);
 };
-// Method that gets content from the IndexedDB database using the idb module
+
+
+// TODO: Add logic for a method that gets all the content from the database
+
+// export const getDb = async () => console.error('getDb not implemented');
+
 export const getDb = async () => {
   console.log('GET from the database');
+  // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
+  // Open up the desired object store.
   const store = tx.objectStore('jate');
+  // Use the .getAll() method to get all data in the database.
   const request = store.get(1);
+  // Get confirmation of the request.
   const result = await request;
-  result
-    ? console.log(':rocket: - data retrieved from the database', result.value)
-    : console.log(':rocket: - data not found in the database');
-  // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
-  return result?.value;
+  console.log('result.value', result);
+  result 
+  ? console.log(":rocket: - data retrieved from the database ", result.value)
+  : console.log(":rocket: - data not found in the database ");
+  return result?.value
 };
+
 initdb();
+
+// import { openDB } from 'idb';
+// const initdb = async () =>
+//   openDB('jate', 1, {
+//     upgrade(db) {
+//       if (db.objectStoreNames.contains('jate')) {
+//         console.log('jate database already exists');
+//         return;
+//       }
+//       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+//       console.log('jate database created');
+//     },
+//   });
+// // Method that takes some content and adds it to the IndexedDB database using the idb module
+// export const putDb = async (content) => {
+//   console.log('PUT to the database');
+//   const jateDb = await openDB('jate', 1);
+//   const tx = jateDb.transaction('jate', 'readwrite');
+//   const store = tx.objectStore('jate');
+//   const request = store.put({ id: 1, value: content });
+//   const result = await request;
+//   console.log(':rocket: - data saved to the database', result.value);
+// };
+// // Method that gets content from the IndexedDB database using the idb module
+// export const getDb = async () => {
+//   console.log('GET from the database');
+//   const jateDb = await openDB('jate', 1);
+//   const tx = jateDb.transaction('jate', 'readonly');
+//   const store = tx.objectStore('jate');
+//   const request = store.get(1);
+//   const result = await request;
+//   result
+//     ? console.log(':rocket: - data retrieved from the database', result.value)
+//     : console.log(':rocket: - data not found in the database');
+//   // Check if a variable is defined and if it is, return it. See MDN Docs on Optional Chaining (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining)
+//   return result?.value;
+// };
+// initdb();
